71b8d3ccb812c1c282f0f948c8ef95d51e4b1cd0
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-03-10 19:28:13 +0530

    Create A2. Student.java

---
+++ Spring Framework/A3. Property injection using p Schema and using value as attribute/A1. springCore.javaDev/A2. Student.java
@@ -0,0 +1,56 @@
+package springCore.javaDev;
+
+public class Student {
+	
+	private int studentId;
+	private String studentName;
+	private String studentAddress;
+	
+	//generate getters setters
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		System.out.println("Setting StudentId");
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		System.out.println("Setting StudentName");
+		this.studentName = studentName;
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		System.out.println("Setting StudentAddress");
+		this.studentAddress = studentAddress;
+	}
+	
+	//generate constructor with field
+	public Student(int studentId, String studentName, String studentAddress) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentAddress = studentAddress;
+	}
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	//generate toString method
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentAddress=" + studentAddress
+				+ "]";
+	}
+	
+	
+	
+	
+
+}
