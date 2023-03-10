9f114764d541a1fdac23d905a966117adc81a3d7
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-03-10 21:02:38 +0530

    Create A1. emp.java

---
+++ Spring Framework/A5. Injecting Collection Types(PRACTICAL)/A1. src-main-java/A1. Collections/A1. emp.java
@@ -0,0 +1,61 @@
+package collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+public class emp {
+	
+	private String name;
+	private List<String> phones; //multiple phone nos ho skte hai
+	private Set<String> addresses; 
+	private Map<String, String> courses; //(courseName,Duration)
+	private Properties props;
+	public Properties getProps() {
+		return props;
+	}
+	public void setProps(Properties props) {
+		this.props = props;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<String> getPhones() {
+		return phones;
+	}
+	public void setPhones(List<String> phones) {
+		this.phones = phones;
+	}
+	public Set<String> getAddresses() {
+		return addresses;
+	}
+	public void setAddresses(Set<String> addresses) {
+		this.addresses = addresses;
+	}
+	public Map<String, String> getCourses() {
+		return courses;
+	}
+	public void setCourses(Map<String, String> courses) {
+		this.courses = courses;
+	}
+	public emp(String name, List<String> phones, Set<String> addresses, Map<String, String> courses) {
+		super();
+		this.name = name;
+		this.phones = phones;
+		this.addresses = addresses;
+		this.courses = courses;
+	}
+	public emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "emp [name=" + name + ", phones=" + phones + ", addresses=" + addresses + ", courses=" + courses + "]";
+	}
+
+}
