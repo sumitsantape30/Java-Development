799f2101b557ddf2eb88dc79d069a635adb223a5
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-03-22 09:32:20 +0530

    Create A1. Demo.java

---
+++ Spring Framework/C20. Spring Expression Language/A2. com.springcore.spel/A1. Demo.java
@@ -0,0 +1,33 @@
+package com.springcore.spel;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component  //object banayega that'll be stored in demo
+public class Demo {
+	
+	@Value("#{22+11}") //isme hum pass kr skte hai expression language
+	private int x;
+	
+	@Value("#{2+ 4 + 56+ 34}")
+	private int y;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	@Override
+	public String toString() {
+		return "Demo [x=" + x + ", y=" + y + "]";
+	}
+	
+	
+
+}
