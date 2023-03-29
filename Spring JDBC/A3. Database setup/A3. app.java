4001364b9bb8139d15d820dce462e8beb71b65b3
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-03-29 17:53:45 +0530

    Create A3. app.java

---
+++ Spring JDBC/A3. Database setup/A3. app.java
@@ -0,0 +1,15 @@
+package com.spring.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //2. hume sabse pehle object chahiye jdbc template ka, hum khudsebhi object bana skte hai but hum nhi banayenge, hum banayenge configuration file
+        
+    }
+}
