1f0f96264f0a5ff5dfee4ea683517f5ed8687f81
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-04-03 15:14:28 +0530

    Create A1. App.java

---
+++ Spring JDBC/A8. Autowiring/A1. com.spring.jdbc/A1. App.java
@@ -0,0 +1,25 @@
+package com.spring.jdbc;
+
+
+import java.util.List;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.spring.jdbc.dao.StudentDao;
+import com.spring.jdbc.entities.Student;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "My program started.........." );   
+        
+        ApplicationContext context = new AnnotationConfigApplicationContext(JdbcConfig.class);
+        
+        StudentDao studentDao= context.getBean("studentDao", StudentDao.class);
+        
+        List<Student> students= studentDao.getAllStudent();
+        for( Student s: students) {
+        	System.out.println(s);
+        }
+    } 
+}
