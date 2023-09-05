e44e552ffa139e556537d3cb388ae476422e7982
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-09-05 17:09:18 +0530

    Update A6. Same function in interface extending multiple classes.java

--- GFG/L1/A6. Same function in interface extending multiple classes.java
+++ GFG/L1/A6. Same function in interface extending multiple classes.java
@@ -19,6 +19,10 @@ public interface L2 {
 
 }
 
+//both L1 and L2 interface have default function named size().
+//sampleInterface is implementing both the above interfaces
+//so it asks us to implement our own size() function which either returns the response of L1 or L2
+
 package Codes;
 
 import java.util.List;
@@ -32,7 +36,7 @@ public interface sampleInterface extends L1, L2 {
 	default int multiply(int a, int b) {
 		return a*b;
 	}
-	//since dono interface mai same method hai so i can define which one to call
+	//since dono interface mai same method hai so i can define which one to call, since both functions are default so overidden function will also be overidden
 	@Override
 	default int size() {
 		return L2.super.size();
