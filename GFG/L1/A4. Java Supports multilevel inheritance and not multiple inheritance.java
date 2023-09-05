6df329b9a53b539d579e21362ced82da015442c6
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-09-05 16:22:42 +0530

    Update A4. Java Supports multilevel inheritance and not multiple inheritance.java

--- GFG/L1/A4. Java Supports multilevel inheritance and not multiple inheritance.java
+++ GFG/L1/A4. Java Supports multilevel inheritance and not multiple inheritance.java
@@ -1,1 +1,54 @@
+package Codes;
+
+public class GrandParent {
+	
+	public void funD() {
+		System.out.println("Inside Grandparent: FunD");
+	}
+	
+	public void funE() {
+		System.out.println("Inside Grandparent: FunE");
+	}
+
+}
+
+
+package Codes;
+
+public class Parent extends GrandParent {
+	
+	public void funcA() {
+		System.out.println("Inside Parent: FuncA");
+	}
+	
+	public void funcB() {
+		System.out.println("Inside Parent: FunB");
+	}
+
+}
+
+
+package Codes;
+
+public class Child extends Parent {
+	
+	public void funcC() {
+		System.out.println("Inside child: funcC");
+	}
+	
+	public void funcA() {
+		System.out.println("Inside child: funcA");
+	}
+	
+	public static void main(String args[]) {
+		
+		//java Support multilevel inheritance like it has formed a chain
+		//child is inheriting from parent and parent is inheriting from grandparent
+		
+		//but multiple inheritance is not possible in java like - Child extends Parent,GrandParent 
+		//one class cannot inherit from multiple classes due ambiguity, soppose same function is defined in parent and grandparent now my child wont understand which function is called parent one or grandparent one
+		
+	}
+
+}
 
