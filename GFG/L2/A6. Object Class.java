f07337d796a3a414405435b9bdda847bfc807660
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-09-06 18:47:29 +0530

    Create A6. Object Class.java

---
+++ GFG/L2/A6. Object Class.java
@@ -0,0 +1,27 @@
+package Codes;
+
+public class Main  {//Every class in java inhiertis from object.java
+	
+	//tum object class se methods yaha overridew kr skte ho according to your needs and can also help you debug code
+	
+	
+	public static void main(String[] args) {
+
+		
+		Integer a= 10;
+		Integer b= 10;
+		System.out.println(a== b);
+		//its capability of java that allows us to initialise Integer without new keyword
+		
+		Integer c = new Integer(10);
+		Integer d = new Integer(20);
+		
+		System.out.println(c == d);
+		
+		//when we write new keyword it allocates a new memory
+		
+	}
+
+}
+
+
