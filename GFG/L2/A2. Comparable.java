82585cd938981c1564b0383f94fd544a0b5a76f0
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-09-06 15:28:03 +0530

    Create A2. Comparable.java

---
+++ GFG/L2/A2. Comparable.java
@@ -0,0 +1,46 @@
+package Codes;
+
+public class Main implements Comparable<Main> {
+	
+	int age;
+	String name;
+	
+	public Main(int age, String name) {
+		this.age= age;
+		this.name= name;
+	}
+	
+	//2. Comparable demands only one object that you want to compare to. coz this an instance function and not an static function so we'll be calling it on some particular object
+	@Override
+	public int compareTo(Main o) {//this o is p2
+		
+		if(this.age == o.age) { //here this is p1. so here age or p1 and p2 is being compared
+			return this.name.compareTo(o.name);
+		}
+		
+		return this.age- o.age; //negative value return hui means p1 chota hai
+	}
+	
+	
+	public static void main(String[] args) {
+
+		Main p1= new Main(5, "abs");
+		Main p2 = new Main(6, "xyz");
+		//1. i want to compare both the students so we'll need to implement comparable interface
+		
+		//3.
+		int result = p1.compareTo(p2); //this line is saying we're comparing p1 with p2
+		
+		if( result < 0) {
+			System.out.println("P1 is smaller");
+		}else if( result > 0) {
+			System.out.println("p2 is smaller");
+		}else {
+			System.out.println("Both are same");
+		}
+		
+	}
+
+	
+
+}
